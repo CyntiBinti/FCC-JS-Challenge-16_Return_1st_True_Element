129710ab4d7851961c465bd1a6cca017a2761c64
Cynthia Mbulu <72040367+CyntiBinti@users.noreply.github.com>
2021-07-07 02:33:27 +0100

    Update file.js

--- file.js
+++ file.js
@@ -1,6 +1,11 @@
 function findElement(arr, func) {
-  let num = 0;
-  return num;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.length[i] % 2) {
+      return arr.length[i];
+    } else {
+      return 'undefined';
+    }
+  }
 }
 
 findElement([1, 2, 3, 4], num => num % 2 === 0);
